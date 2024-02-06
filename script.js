9cfe736e40fdcb5ca1afa325d1ed27cff6a90a5c
Mysokapg <poupeney.gi@gmail.com>
2024-02-06 11:25:51 +0100

    ajout du js et du header

---
+++ script.js
@@ -0,0 +1,11 @@
+let clap=document.querySelector("input");
+let countclap =0;
+let truebff ="";
+
+
+if (clap == true) {
+    countclap +=1;
+    if (countclap ==100){
+        truebff = "TU EST UN VRAI BESTO FRIENDO!!";
+    }
+}
